16b6f6d3fdb37031ecd93b752521043525df8a0b
周立强 <luziang-1024@163.com>
2020-08-06 11:55:09 +0800

    1.0

---
+++ src/Controllers/BaseController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smartwell
+ * Date: 2018/12/13
+ * Time: 上午10:28
+ */
+
+namespace Smartwell\Controllers;
+
+use Illuminate\Routing\Controller;
+
+class BaseController extends Controller
+{
+
+    public function message($data) {
+        $result = array(
+            "code" => 200,
+            "message" => '',
+            'data' => $data,
+        );
+        return json_encode($result, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
+    }
+
+    public function errorMessage($code, $message,$data) {
+        $result = array(
+            "code" => $code,
+            "message" => $message,
+            'data' => $data,
+        );
+        return json_encode($result, JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
+    }
+}
