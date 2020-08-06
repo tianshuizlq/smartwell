98a1854fa27cf686e8139322da56fe1872f4b60a
周立强 <luziang-1024@163.com>
2020-08-06 14:26:32 +0800

    修复bug

--- src/SmartwellServicesProvider.php
+++ src/SmartwellServicesProvider.php
@@ -9,9 +9,7 @@ class SmartwellServicesProvider extends ServiceProvider
 
     public function boot()
     {
-        if($this->runningInConsole()) {
-            $this->published([__DIR__ . '/../config/smartwell.php' => config_path('smartwell.php'),]);
-        }
+        $this->published([__DIR__ . '/../config/smartwell.php' => config_path('smartwell.php'),]);
     }
 
     public function register()
